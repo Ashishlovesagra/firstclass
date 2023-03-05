0e0466a4a5e6d978a9b9fa8af28bfe4f63aa090c
ashish <ashishlovesagra@gmail.com>
2023-03-06 05:26:04 +0530

    react-handson-5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MyComponent from './Hoc Example/MyComponent';
+import PureComp from './Pure Component/PureComp';
 
 
 
 function App() {
   return (
     <div className="App">
+      <h1>Pure Component Example</h1>
+      <PureComp/>
+      <h1>HOC Component Example</h1>
       <MyComponent />
     </div>
   );
